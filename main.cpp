13c3514a86019b5ded6194539d2f191a37681403
llildin <132454924+llildin@users.noreply.github.com>
2026-04-02 10:59:12 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+using byte = unsigned long long;
+
+int main()
+{
+	int Count = 0;
+	byte Result = 0;
+
+	cout << "Number Count : ";
+	cin >> Count;
+
+	byte* InNumber = new byte[Count];
+	byte* OutNumber = new byte[Count];
+
+	for (int i = 0; i < Count; i++)
+	{
+		cin >> InNumber[i];
+		OutNumber[i] = 1;
+
+		for (; InNumber[i] > 1;)
+		{
+			InNumber[i] /= 2;
+			OutNumber[i] *= 2;
+		}
+		OutNumber[i] *= 2;
+	}
+
+	for (int i = 0; i < Count; i++)
+	{
+		cout << OutNumber[i] << endl;
+		Result = Result ^ OutNumber[i];
+	}
+
+	cout << Result;
+
+	delete[] InNumber;
+	InNumber = nullptr;
+	delete[] OutNumber;
+	OutNumber = nullptr;
+
+	return 0;
+}
